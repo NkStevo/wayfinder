9f0104533ae4f1e21685f6e2a3c584093b48a530
Alevalam <aav32014@mymail.pomona.edu>
2017-07-16 06:08:47 -0700

    transitions

--- static/js/helpers.js
+++ static/js/helpers.js
@@ -1,16 +1,57 @@
-  $("#main").on("swiperight",function nextRight(){
-  //  $(this).css({'background-color': 'blue'})
-    $(this).animate({ height: 0, opacity: 0 }, 'slow');
-    //$("#holderDiv").append($(this))
+var selectedDiv = $("#main").children()[0];
 
-  });
+  $("#wrapper").on("swiperight",function nextRight(){
+    $(this).animate({
+       opacity: 0, // animate slideUp
+       marginLeft: '+300px'
+     }, 'fast', 'linear', function() {
+       $.when($(this).fadeOut("fast", function(){
+         opacity: 0
+         $("#holderDiv").append(selectedDiv)
+         next(selectedDiv);
+         selectedDiv = $("#main").children()[0];
+
+
+       })).done($(this).fadeIn("fast", function(){
+         $(this).css({"opacity": "", "margin-left": "", "display": "", "transition":".25s ease-in"})
+       }));
+     });
+   });
 
 
 
-  $("#main").on("swipeleft",function nextLeft(){
-    $(this).css({'background-color': 'red'})
-  });
+
+
+   $("#wrapper").on("swipeleft",function nextLeft(){
+     $(this).animate({
+        opacity: 0, // animate slideUp
+        marginLeft: '-300px'
+      }, 'fast', 'linear', function() {
+        $.when($(this).fadeOut("fast", function(){
+          opacity: 0
+          $("#holderDiv").append(selectedDiv)
+          next(selectedDiv);
+          selectedDiv = $("#main").children()[0];
+
+
+        })).done($(this).fadeIn("fast", function(){
+          $(this).css({"opacity": "", "margin-left": "", "display": "", "transition":".25s ease-in"})
+        }));
+      });
+    });
+
 
   function next(currentDiv){
+    switch(currentDiv.id){
+      case "evan":
+        $("#main").append($("#ale"))
+        break;
+      case "ale":
+        $("#main").append($("#steve"))
+        break;
+      case "steve":
+        $("#main").append($("#evan"))
+        break;
 
+    }
   }
